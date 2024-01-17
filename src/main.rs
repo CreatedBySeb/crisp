b34fe3b85c46782e76de99e7879d0c33d819229b
Sébastien Dunne Fulmer <createdbyseb@gmail.com>
2024-01-17 17:02:50 +0000

    Load provided ROM into memory

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,35 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use std::env;
+use std::fs;
 use std::time::Duration;
 
+const PROGRAM_OFFSET: usize = 0x200;
+const MEMORY_SIZE: usize = 4096;
+const PROGRAM_MAX_SIZE: usize = MEMORY_SIZE - PROGRAM_OFFSET;
+
+fn read_rom(memory: &mut [u8; MEMORY_SIZE], rom_path: &String) {
+    let contents = fs::read(rom_path).unwrap();
+    assert!(
+        contents.len() <= PROGRAM_MAX_SIZE,
+        "ROM exceeded max size {}B",
+        PROGRAM_MAX_SIZE
+    );
+
+    let length = contents.len();
+    memory[PROGRAM_OFFSET..PROGRAM_OFFSET + length].copy_from_slice(&contents);
+    println!("Read in ROM of length {}B", length);
+}
+
 fn main() {
+    let mut memory: [u8; MEMORY_SIZE] = [0; MEMORY_SIZE];
+    let mut pc = PROGRAM_OFFSET;
+
+    let args: Vec<String> = env::args().collect();
+    let rom_path = &args[1];
+    read_rom(&mut memory, rom_path);
+
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
